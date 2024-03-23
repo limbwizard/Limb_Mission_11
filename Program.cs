957a0a6724f5944a5d0f6c66b7ddb9aa54517d02
Isaac Limb <isaacdalelimb@gmail.com>
2024-03-22 22:31:29 -0600

    Major changes

--- Program.cs
+++ Program.cs
@@ -31,7 +31,11 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    "pagination",
+    "Books/{pageNum}",
+    new {Controller = "Home", action = "Index"}
+    );
+
+app.MapDefaultControllerRoute();
 
 app.Run();
